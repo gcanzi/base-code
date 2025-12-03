ea78b6a9396a3feb77e5723cb7eaba8dce9dab52
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2025-12-03 19:42:16 -0300

    practing

---
+++ idades.py
@@ -0,0 +1,11 @@
+print("Dados da primeira pessoa:")
+name1 = str(input("Nome: "))
+age1 = int(input("Idade: "))
+
+print("Dados da segunda pessoa:")
+name2 = str(input("Nome: "))
+age2 = int(input("Idade: "))
+
+media = (age1 + age2) /2
+
+print(f"A idade média de {name1} e {name2} é de {media} anos.")
